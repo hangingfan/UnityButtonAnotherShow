10fcca5b99b2549ecdfb6f464135ba65090a09ed
Lu Taian <lutaian@fantablade.com>
2017-10-18 15:48:53 +0800

    [modify] in traditional ways,we show add button clickevent on every buttons, if we have too many buttons to control. we would not know exactly the buttons's precise position. So I made a trick to let all the button click on the parent script below

---
+++ test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using Chronos.Reflection;
+
+[System.Serializable]
+public class LocalizationItem
+{
+    public Button key;
+    public Button.ButtonClickedEvent value;
+
+    public void Init()
+    {
+        key.onClick = value;
+    }
+}
+public class test : MonoBehaviour {
+
+    [SerializeField]
+    private LocalizationItem CreateButton;
+
+    void Start()
+    {
+        CreateButton.Init();
+    }
+
+    public void HHHHHH()
+    {
+        Debug.Log("XXXXXXXXXX");
+    }
+}
